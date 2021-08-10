4244842706d2123b9f65515d4a1bc680385813d5
Heidi Hlavinka <hchlav@gmail.com>
2021-08-10 11:10:37 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
 # code your #position_taken? method here!
+require 'pry'
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end    
+end
+
+
+# This passes tests, but breaks when passed in a board with some positions already taken.
+#   board[0] != " " || ""
+#   board[0] != nil
+#   board[0] == "X"
+#   board[8] == "O"    
